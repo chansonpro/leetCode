95001fbecdbb35b88d971bbd2dfbac9bd90a2d7f
Chanson <chansonpro@outlook.com>
2017-11-02 09:59:21 +0800

    使用hash table，two sum

---
+++ src/main/java/easy/TwoSum.java
@@ -0,0 +1,18 @@
+package easy;
+
+/**
+ * User: chanson-pro
+ * Date-Time: 2017-11-2 9:56
+ * Description:   // 使用hash table
+ * Given an array of integers,
+ * return indices of the two numbers such that they add up to a specific target.
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+
+    }
+
+}
